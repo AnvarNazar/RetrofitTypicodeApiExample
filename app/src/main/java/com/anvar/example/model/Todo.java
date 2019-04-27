22612538f7d279ae93356e5434ca7a9bea567db9
Anvar Nazar <anvarn4@gmail.com>
2019-04-28 01:00:08 +0530

    project created and created Todo model

---
+++ app/src/main/java/com/anvar/example/model/Todo.java
@@ -0,0 +1,14 @@
+package com.anvar.example.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Todo {
+    @SerializedName("id")
+    public long id;
+    @SerializedName("userId")
+    public long userId;
+    @SerializedName("title")
+    public String title;
+    @SerializedName("isCompleted")
+    public boolean isCompleted;
+}
